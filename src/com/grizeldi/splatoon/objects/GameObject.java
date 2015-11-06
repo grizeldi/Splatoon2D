78d3b3782f4441a692743527b3700bda5164f9fe
grizeldi <grizeldi@gmail.com>
2015-11-06 18:55:25 +0100

    Re-uploaded source

--- src/com/grizeldi/splatoon/objects/GameObject.java
+++
@@ -1,8 +0,0 @@
-package com.grizeldi.splatoon.objects;
-
-/**
- * Flag interface for game objects
- */
-public abstract class GameObject {
-    public float x, y, rotation;
-}
--- src/com/grizeldi/splatoon/updating/UpdateAble.java
+++
@@ -1,7 +0,0 @@
-package com.grizeldi.splatoon.updating;
-
-import org.newdawn.slick.GameContainer;
-
-public interface UpdateAble {
-    public void update(GameContainer container, float tpf);
-}
