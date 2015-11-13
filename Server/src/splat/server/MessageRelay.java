0faffffcd86af82bcc43c95d8a001dc1f732698c
grizeldi <grizeldi@gmail.com>
2015-11-13 19:45:32 +0100

    Finished MessageRelay

--- Server/src/splat/server/MessageRelay.java
+++ Server/src/splat/server/MessageRelay.java
@@ -14,6 +14,7 @@ public class MessageRelay {
     private ClientManager clientManager;
     private List<BufferedReader> inputs = new ArrayList<BufferedReader>();
     private List<PrintWriter> outputs = new ArrayList<PrintWriter>();
+    private List<ListenerThread> threads = new ArrayList<ListenerThread>();
 
     public MessageRelay(ClientManager clientManager, Main main) throws IOException{
         this.clientManager = clientManager;
@@ -21,29 +22,37 @@ public class MessageRelay {
         for (Socket s : clientManager.clientConnections){
             inputs.add(new BufferedReader(new InputStreamReader(s.getInputStream())));
             outputs.add(new PrintWriter(new OutputStreamWriter(s.getOutputStream())));
+            threads.add(new ListenerThread(inputs.get(inputs.size() - 1)));
+        }
+    }
+
+    public void relayToAll(String s) throws WriteFailedException{
+        for (PrintWriter out : outputs){
+            out.println(s);
+            if (out.checkError())
+                throw new WriteFailedException(out);
         }
     }
 
     public class ListenerThread extends Thread{
-        private MessageRelay relay;
         private BufferedReader buff;
 
-        public ListenerThread(MessageRelay relay, BufferedReader reader) {
-            this.relay = relay;
+        public ListenerThread(BufferedReader reader) {
             buff = reader;
             this.start();
         }
 
-        public ListenerThread(MessageRelay relay, int index) {
-            this(relay, relay.inputs.get(index));
+        public ListenerThread(int index) {
+            this(inputs.get(index));
         }
 
         @Override
         public void run() {
             super.run();
             try {
-                while (!this.relay.main.shouldExit) {
-
+                while (!main.shouldExit) {
+                    String s = buff.readLine();
+                    relayToAll(s);
                 }
             }catch (Exception e){
                 System.err.println("Listener thread failed. Shutting this client down.");
